527ec142375e99bcd83f81babe73fc84b85811ae
ToniPF <toni.pafe@gmail.com>
2019-02-26 10:48:51 +0100

    Created first view.

--- paquipalla/paquipalla/urls.py
+++ paquipalla/paquipalla/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import envelopes.views as ev
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ev.mainpage, name="HomePage")
 ]
