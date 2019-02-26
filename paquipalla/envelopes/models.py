43293fad7fecff0bcefaefd5afd661868d1413db
ToniPF <toni.pafe@gmail.com>
2019-02-26 10:32:22 +0100

    Added to Envelope and Company str function

--- paquipalla/envelopes/models.py
+++ paquipalla/envelopes/models.py
@@ -6,8 +6,14 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return "{}".format(self.name)
+
 
 class Envelope(models.Model):
     amount = models.IntegerField()
     motive = models.CharField(max_length=100)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} - {}".format(self.company.name, self.motive)
